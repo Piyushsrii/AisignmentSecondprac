b8a80f125acf918d006d3b6a1cbec03a882e1381
piyushsrii <piyushnamo888@gmail.com>
2020-07-24 13:57:18 +0530

    commit in uc2

--- Secondassignmnetof11.sh
+++ Secondassignmnetof11.sh
@@ -1,11 +1,19 @@
 Welcome
 
-#!/bin/bash -x
+#!/bin/bash -x        #UserCase------>2
 
+win=0;
+bats=0
+for((i=1;i<=10;i++))
+do
 randomvar=$((RANDOM%2))
 if [[ $randomvar == 1 ]]
 then
-echo "Head win"
+((win++))
+echo "Head $win"
 else
-echo "Tail win"
+((bats++))
+echo "Tail $bats"
 fi
+done
+
